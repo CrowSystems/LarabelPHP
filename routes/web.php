7071302330a7c47aabe8a7c69c40ebc36709475c
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-12-19 13:31:35 -0600

    carga de git

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,14 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::view('/', 'index');
 
-Route::get('/', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function(){
+    return Inertia::render('Dashboard');
+})->name('dashboard');
+
+Route::middleware(['']);
+/*Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
@@ -33,3 +29,4 @@ Route::middleware([
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });
+*/
